85c1a4b6aa21330f06530951f1d094ca92f04381
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-08-06 23:16:19 -0400

    Delete api/server/config directory

--- api/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-console.log("Mongoose is working!");
-
-mongoose.connect("mongodb://localhost/beblog", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => console.log("That's what I wanna see!"))
-    .catch(err => console.log("Nope try again....", err));
