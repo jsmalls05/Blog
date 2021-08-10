9ddb2cab5db85929dfce325908cd05ba4a62cb5f
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-08-09 22:43:28 -0400

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -19,6 +19,7 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
+        useFindAndModify: true,
     })
     .then(console.log("Connected to MongoDB"))
     .catch(err => console.log(err));
