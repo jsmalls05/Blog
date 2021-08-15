4dea6e29b38837fb401eb10719da14f932d4191f
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-08-14 22:35:21 -0400

    Update Context.jsx

--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -5,7 +5,7 @@ const INITIAL_STATE = {
     user: JSON.parse(localStorage.getItem("user")) || null,
     isFetching: false,
     error: false,
-};
+}; 
 
 export const Context = createContext(INITIAL_STATE);
 
