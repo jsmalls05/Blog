029bb079425f6e371866629f08c17d84259d4290
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2021-08-29 20:58:11 -0400

    Update Context.jsx

--- client/src/context/Context.jsx
+++ client/src/context/Context.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useReducer } from "react"
-import Reducer from "./Reducer";
+import Reducer from "./Reducer"; 
 
 const INITIAL_STATE = {
     user: JSON.parse(localStorage.getItem("user")) || null,
